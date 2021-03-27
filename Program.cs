643d06ed6cc32fd2b0834081e028d21c0b4c000c
Jorge <jepers2021@gmail.com>
2021-03-27 18:38:44 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace A133590.Ejercicio16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 16.");
+            Console.WriteLine("Programa que determina si un ingreso es un número entero.");
+            Console.Write("Por favor, ingrese un número: ");
+            int ingreso;
+            bool exito = Int32.TryParse(Console.ReadLine(), out ingreso);
+            if (exito)
+            {
+                Console.WriteLine("El ingreso puede ser interpretado como un número entero.");
+            }
+            else
+            {
+                Console.WriteLine("El ingreso no puede ser interpretado como un número entero.");
+            }
+
+            Console.WriteLine("Presione cualquier tecla para continuar..");
+            Console.ReadKey();
+        }
+    }
+}
